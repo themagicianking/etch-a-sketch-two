4cf56b5ebf6e0d03c47f2928c51762419568616b
themagicianking <tmoonder@gmail.com>
2023-04-04 19:27:49 -0400

    Refactored the code to be more efficient and get rid of the create row function.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,19 @@
 const grid = document.createElement("div");
-grid.style.border = "thick solid black";
+//grid.style.border = "thick solid black";
+grid.classList.add("grid");
 document.body.appendChild(grid);
-
-let box;
 
 function createBox () {
     box = document.createElement("div");
-    box.style.border = "thin dotted green";
+    box.classList.add("box");
+    //box.style.border = "thin dotted green";
     box.textContent = "i'm a box!";
-};
-
-function createRow () {
-    const row = document.createElement("div");
-    row.style.border = "thin solid red";
-    row.textContent = "i'm a row!";
-    grid.appendChild(row);
-    for (let i = 1; i <= 16; i++) {
-        createBox();
-        row.appendChild(box);
-    }
+    grid.appendChild(box);
 };
 
 function createGrid () {
-    for (let i = 1; i <= 16; i++) {
-        createRow();
+    for (let i = 1; i <= 256; i++) {
+        createBox();
     }
 }
 
