70b9e12195dcfdf63cbc5cb59b0d9536be057251
themagicianking <tmoonder@gmail.com>
2023-04-04 19:11:38 -0400

    Refactored the code so that the grid is created in a way that will be conducive to flexbox.

--- javascript.js
+++ javascript.js
@@ -2,15 +2,22 @@ const grid = document.createElement("div");
 grid.style.border = "thick solid black";
 document.body.appendChild(grid);
 
+let box;
+
 function createBox () {
-    const box = document.createElement("div");
-    box.textContent = "i'm a box!"
-    grid.appendChild(box);
+    box = document.createElement("div");
+    box.style.border = "thin dotted green";
+    box.textContent = "i'm a box!";
 };
 
 function createRow () {
+    const row = document.createElement("div");
+    row.style.border = "thin solid red";
+    row.textContent = "i'm a row!";
+    grid.appendChild(row);
     for (let i = 1; i <= 16; i++) {
-    createBox();
+        createBox();
+        row.appendChild(box);
     }
 };
 
