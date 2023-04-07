b57a491325ea954cf05f2af762a984e54c2c32bf
themagicianking <tmoonder@gmail.com>
2023-04-07 12:38:33 -0400

    Completed the project, implementing the use of the user's input to create a bigger or smaller grid.

--- javascript.js
+++ javascript.js
@@ -2,14 +2,18 @@ const grid = document.createElement("div");
 grid.classList.add("grid");
 document.body.appendChild(grid);
 
+let x = 16;
+
 function createBox () {
     box = document.createElement("div");
     box.classList.add("box");
+    box.style.width = `${500/x}px`;
+    box.style.height = `${500/x}px`;
     grid.appendChild(box);
 };
 
 function createGrid () {
-    for (let i = 1; i <= 256; i++) {
+    for (let i = 1; i <= (x * x); i++) {
         createBox();
     }
 };
@@ -17,18 +21,27 @@ function createGrid () {
 createGrid();
 
 let allBoxes = document.querySelectorAll("div.box");
-console.log(allBoxes);
 
-for ( let i = 0; i < allBoxes.length; i++) {
-    allBoxes[i].addEventListener("mouseover", colorChange);
-    function colorChange () {
-        allBoxes[i].classList.add("color");
+function etch () {
+    for (let i = 0; i < allBoxes.length; i++) {
+        allBoxes[i].addEventListener("mouseover", colorChange);
+        function colorChange () {
+            allBoxes[i].classList.add("color");
+        };
     };
 };
 
+etch();
+
 function reset() {
-    let x = prompt("How many squares would you like the new grid to have?");
-    for ( let i = 0; i < allBoxes.length; i++) {
-        allBoxes[i].classList.remove("color");
+    for (let i = 0; i < allBoxes.length; i++) {
+        allBoxes[i].remove();
     };
+    x = prompt("How many squares would you like the new grid to have per row?");
+    if (x > 100) {
+        x = 100;
+    };
+    createGrid();
+    allBoxes = document.querySelectorAll("div.box");
+    etch();
 };
